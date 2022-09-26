c28d41a39c6224ca9b5ffd9107442ef6ef54dbe2
hugoc <hugococa2004@gmail.com>
2022-09-26 13:39:40 +0200

    Version 0.0.1 (not final)

---
+++ app/src/main/java/com/hugodev/rgbtester/MainActivity.java
@@ -0,0 +1,35 @@
+package com.hugodev.rgbtester;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent otherActivity = new Intent(getApplicationContext(), TestHome.class);
+            startActivity(otherActivity);
+            finish();
+        });
+
+        AdView mAdView = findViewById(R.id.adView_home);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        MobileAds.initialize(this, initializationStatus -> {
+        });
+        }
+    }
+
+
