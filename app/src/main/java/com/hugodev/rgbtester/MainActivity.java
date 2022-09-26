4284e7957b72a7477c4d697f49eee5c243cc4d6c
hugoc <hugococa2004@gmail.com>
2022-09-26 17:59:47 +0200

    Version 0.0.1 (not final)

--- app/src/main/java/com/hugodev/rgbtester/MainActivity.java
+++ app/src/main/java/com/hugodev/rgbtester/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.ads.MobileAds;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
--- app/src/main/java/com/hugodev/rgbtester/TestHome.java
+++ app/src/main/java/com/hugodev/rgbtester/TestHome.java
@@ -1,38 +1,34 @@
 package com.hugodev.rgbtester;
 
+import static android.graphics.Color.rgb;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.content.Intent;
-import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
-
-
-import java.util.Timer;
-import java.util.TimerTask;
-
-
 
 public class TestHome extends AppCompatActivity {
 
     @Override
-    public void NextFrame(View view) {
-               view.setBackgroundResource(R.color.black);
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_home);
-        Button button = findViewById(R.id.button2);
-
-        button.setOnClickListener(view -> {
-            Intent otherActivity = new Intent(getApplicationContext(), TestHome.class);
-            startActivity(otherActivity);
-            finish();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+            getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
+        }        setContentView(R.layout.activity_test_home);
+        LinearLayout fond = (LinearLayout) findViewById(R.id.fond);
+        fond.setOnClickListener(view -> {
+            fond.setBackgroundColor(rgb(0, 255, 0));
         });
 
-    };
+    }
+
 }
